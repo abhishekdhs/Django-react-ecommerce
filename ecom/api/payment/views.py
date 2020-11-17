451ed5b3fde31d41b68c79922f8d8ad63310e20b
Abhishek Patel <34875711+abhishekdhs@users.noreply.github.com>
2020-11-17 17:55:33 +0530

    Update views.py

--- ecom/api/payment/views.py
+++ ecom/api/payment/views.py
@@ -12,9 +12,9 @@ import braintree
 gateway = braintree.BraintreeGateway(
     braintree.Configuration(
         braintree.Environment.Sandbox,
-        merchant_id="3fy8tzdbr3rpgq8h",
-        public_key="rfb9rdv4yq3d92qm",
-        private_key="7e3e7363714aa1baeeb4bdeb1bc9a2eb"
+        merchant_id="################",
+        public_key="###############",
+        private_key="##############"
     )
 )
 
